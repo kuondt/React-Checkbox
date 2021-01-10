efef1690d099ffd26b840585a4cc506414b2eb04
Đào Tuấn Cường <cuong.263@gmail.com>
2021-01-10 23:31:21 +0700

    check item

--- src/App.js
+++ src/App.js
@@ -1,52 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
-import React, { useState, useReducer } from "react";
+import React, { useState, useEffect } from "react";
 
-const Checkbox = ({ fnClick, fnChange, title = "", checked = false }) => (
-  <label>
-    <input
-      onClick={e => {
-        if (fnClick !== undefined) fnClick(e.target.checked);
-      }}
-      onChange={e => {
-        if (fnChange !== undefined) fnChange(e.target.checked);
-      }}
-      type="checkbox"
-      checked={checked}
-    />
-    {" Checkbox " + title}
-  </label>
-);
+const Products = [
+  { Id: "product001", name: "Apple", isDeleted: false, checked: false },
+  { Id: "product002", name: "Samsung", isDeleted: false, checked: false },
+  { Id: "product003", name: "Oppo", isDeleted: false, checked: false },
+  { Id: "product004", name: "Xiaomi", isDeleted: false, checked: false },
+];
+
+const Product = ({product, returnData}) => {
+  const [checked, setChecked] = useState(false);
+
+  // useEffect(() => {
+  //   setChecked(product.checked)
+  // })
+  const handleChecked = () => { 
+    setChecked(!checked);
+    //console.log(checked);
+    returnData(product.Id)
+  }
+
+  return (
+    <div>
+      <div>
+        <input type="checkbox" name="" id="" checked={checked} onChange={() => handleChecked()} />
+      </div>
+      <div>
+        Id: {product.Id}
+      </div>
+      <div>
+        Name: {product.name}
+      </div>
+    </div>
+  )
+}
 
 const App = () => {
-  const initialState = {
-    click: false,
-    change: false
-  };
+  const [listProduct, setListProduct] = useState(Products);
 
-  const reducer = (state, action) => ({ ...state, ...action });
-  const [state, setState] = useReducer(reducer, initialState);
+  useEffect(() => {
+    setListProduct(Products);
+  });
 
-  const clearFilter = () => setState(initialState);
+  //console.log(listProduct);
+
+  const handleReturnData = (idProduct) => {
+    let prod = listProduct
+    let objIndex  = listProduct.findIndex(x => x.Id == idProduct)
+    prod[objIndex].checked = !prod[objIndex].checked;  
+    console.log("prod",listProduct);
+    setListProduct(prod)
+
+  }
+
+  const handleSubmit = () => {
+    let prodChecked = listProduct.filter(x => x.checked == true);
+    console.log(prodChecked);
+  }
+
+  const handleCheckAll = () => {
+    let objIndex  = listProduct.map((prod, index) => (prod.checked = true) )
+    console.log("Check",objIndex);
+    //setListProduct(checkAll);
+  }
+
   return (
-    <div className="App">
-      <button onClick={() => clearFilter()}>Limpiar</button>
-      <br />
-      <Checkbox
-        title="Click"
-        fnClick={v => setState({ click: v })}
-        checked={state.click}
-      />
-      <br />
-      <Checkbox
-        title="Change"
-        fnChange={v => setState({ change: v })}
-        checked={state.change}
-      />
-      <br />
-      click: {state.click ? "true" : "false"}
-      <br />
-      change: {state.change ? "true" : "false"}
+    <div style={{ padding: '20px' }}>
+      <button type="submit" onClick={() => handleCheckAll()}>Check All</button>
+      {
+        listProduct.map((product, index) => (
+          <Product product={product} returnData={(id) => handleReturnData(id)} />
+        ))
+      }
+      <button type="submit" onClick={() => handleSubmit()}>Đồng ý</button>
+      
     </div>
   );
 };
