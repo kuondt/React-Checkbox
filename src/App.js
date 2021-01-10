8fc24f368da8a99495ed805ef9d4678fe118c80b
Đào Tuấn Cường <cuong.263@gmail.com>
2021-01-10 21:19:34 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useReducer } from "react";
 
-function App() {
+const Checkbox = ({ fnClick, fnChange, title = "", checked = false }) => (
+  <label>
+    <input
+      onClick={e => {
+        if (fnClick !== undefined) fnClick(e.target.checked);
+      }}
+      onChange={e => {
+        if (fnChange !== undefined) fnChange(e.target.checked);
+      }}
+      type="checkbox"
+      checked={checked}
+    />
+    {" Checkbox " + title}
+  </label>
+);
+
+const App = () => {
+  const initialState = {
+    click: false,
+    change: false
+  };
+
+  const reducer = (state, action) => ({ ...state, ...action });
+  const [state, setState] = useReducer(reducer, initialState);
+
+  const clearFilter = () => setState(initialState);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => clearFilter()}>Limpiar</button>
+      <br />
+      <Checkbox
+        title="Click"
+        fnClick={v => setState({ click: v })}
+        checked={state.click}
+      />
+      <br />
+      <Checkbox
+        title="Change"
+        fnChange={v => setState({ change: v })}
+        checked={state.change}
+      />
+      <br />
+      click: {state.click ? "true" : "false"}
+      <br />
+      change: {state.change ? "true" : "false"}
     </div>
   );
-}
-
+};
 export default App;
